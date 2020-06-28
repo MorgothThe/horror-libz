6884177df9b0f7b80de57f30329e29f422fcb457
paulinakowalinska <pkowalinska67@gmail.com>
2020-06-28 15:59:05 +0200

    initialize spring project

---
+++ src/main/java/com/choelinska/horrorlibz/HorrorLibzApplication.java
@@ -0,0 +1,13 @@
+package com.choelinska.horrorlibz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorrorLibzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorrorLibzApplication.class, args);
+	}
+
+}
